57959b147cb2b58c5057248ed590a9223550298d
kebishaa <kbkibatu@gmail.com>
2022-08-24 00:40:36 +0300

    added plot_box_multi function

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -105,6 +105,20 @@ class exploration:
         plt.title(title, size=20)
         plt.xticks(rotation=75, fontsize=14)
         plt.show()
+    def plot_box_multi(df: pd.DataFrame, x_col: str, y_col: str, title: str) -> None:
+        '''
+        # box_multi: Plot a box plot.
+        # df: dataframe to be plotted
+        # x_col: x-axis column
+        # y_col: y-axis column
+        # title: Title of the plot
+        '''
+        plt.figure(figsize=(12, 7))
+        sns.boxplot(data=df, x=x_col, y=y_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=75, fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.show()
 
 
          
