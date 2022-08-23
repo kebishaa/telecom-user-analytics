f658951b92777355eb22f4a996ba06fe1e9c469a
kebishaa <kbkibatu@gmail.com>
2022-08-24 00:17:32 +0300

    added plot_scatter function

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -37,5 +37,20 @@ class exploration:
         sns.heatmap(correlation)
         plt.title(title, size=18, fontweight='bold')
         plt.show()
+    def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+        '''
+        # scatter: Plot data as a scatter plot.
+        # df: dataframe to be plotted
+        # x_col: x-axis column
+        # y_col: y-axis column
+        # title: Title of the plot
+        # hue: hue column
+        '''
+        plt.figure(figsize=(12, 7))
+        sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
+        plt.title(title, size=20)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.show()
 
          
