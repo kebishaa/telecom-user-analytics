a513e677441b65bfffec1fb752730f5764b9bb48
kebishaa <kbkibatu@gmail.com>
2022-08-24 22:50:10 +0300

    modified convert_to_numbers function

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -14,3 +14,6 @@ class data_preProcessing_script:
     def drop_duplicates(self) -> pd.DataFrame:
         droped = self.df[self.df.duplicated()].index
         return self.df.drop(index=droped, inplace=True)
+def convert_to_numbers(self) -> pd.DataFrame:
+        self.df = self.df.apply(pd.to_numeric, errors='coerce')
+        return self.df
