fdcf498c1144f2465cdf803310192cbcbaa0347c
kebishaa <kbkibatu@gmail.com>
2022-08-24 23:16:37 +0300

    added missing_percentage function

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -56,3 +56,15 @@ def colums_WithMissingValue(self):
                 miss.append(dff.index[summ])
             summ += 1
         return miss
+def get_column_based_missing_percentage(self):
+        col_null = self.df.isnull().sum()
+        total_entries = self.df.shape[0]
+        missing_percentage = []
+        for col_missing_entries in col_null:
+            value = str(
+                round(((col_missing_entries/total_entries) * 100), 2)) + " %"
+            missing_percentage.append(value)
+
+        missing_df = pd.DataFrame(col_null, columns=['total_missing_values'])
+        missing_df['missing_percentage'] = missing_percentage
+        return missing_df
